6fd8972e325236ec8101be6ec08080a0bcb9e21e
YousraDesoukey <yousradesoukey@hotmail.com>
2017-10-02 23:32:25 +0200

    django website

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,21 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # /music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+    # /music/<album_id>/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+    # /music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
+
+
 ]
